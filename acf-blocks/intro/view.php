9a9865b2d693539bb16b74dd529b5028b48e8709
Tanguy Pinel <tanguypinel@gmail.com>
2023-09-26 15:57:24 +0200

    Coqu

--- acf-blocks/intro/view.php
+++ acf-blocks/intro/view.php
@@ -2,9 +2,13 @@
     $fields = get_fields();
     extract($fields);
     ?>
-    <section class="intro">
+    <section class="intro coqu etiq">
         <div class="content">
-            <p class="texte"><?php echo $zone_de_texte; ?></p>
-            <?php echo wp_get_attachment_image($image, 'full'); ?>
+            <div class="logo sew"> <?php the_custom_logo() ?> </div>
+            <div class="texte">
+                <?php echo $zone_de_texte; ?>
+                <a class="button" href="#">Découvrez nos tissus</a>
+            </div>
+            <div class="image"><?php echo wp_get_attachment_image($image, 'full'); ?></div>
         </div>
     </section>
--- front-page.php
+++ front-page.php
@@ -7,8 +7,6 @@ get_header();
 
 ?>
 
-<code>front-page.php</code>
-
 <?php
     the_content();
 ?>
