aa59e2f3754daf3231d370ac7271cf4828f640f5
Benjamin Gosset <benjamin.gosset@pixelea.fr>
2023-10-23 10:22:38 +0200

    Changes footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	</main><!-- /#main-content -->
 <footer role="contentinfo">
 	<div class="bottom-bar">
-		<p>&copy; Studio Pixelea - <?php echo date("Y"); ?></p>
+	<p><a href="<?php echo site_url( '/mentions-legales/' );?>">Mentions légales</a> - <a href="<?php echo site_url( '/protection-de-vos-donnees/' );?>">Protection de vos données</a> - &copy; Studio Pixelea - <?php echo date("Y"); ?></p>
 	</div>
 </footer>
 <?php wp_footer(); ?>
